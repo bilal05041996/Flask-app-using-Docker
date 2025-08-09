1043ec807731507efdfce6e24c8ff584df76726a
Ubuntu <ubuntu@ip-172-31-2-73.ap-south-1.compute.internal>
2025-08-09 07:01:15 +0000

    Updated environment variable configuration

--- app.py
+++ app.py
@@ -1,20 +1,24 @@
+import os
 from flask import Flask
 
 app = Flask(__name__)
 
+# Read environment variable, set default if not provided
+MESSAGE = os.getenv("WELCOME_MSG", "Hello from Dockerized Flask App!")
+
 @app.route('/')
 def home():
-    return "Hello from Dockerized Flask App!"
+    return MESSAGE
 
-#Route: About
 @app.route('/about')
 def about():
-    return "This is a simple flask app running inside a Docker container"
-#Route: Status
+    return "This is a simple Flask app running inside a Docker container."
+
 @app.route('/status')
 def status():
-    return "Status: App is running fine"
+    return "Status: App is running fine."
 
 if __name__ == '__main__':
+    # Run Flask on 0.0.0.0 so it's accessible from outside container
     app.run(host='0.0.0.0', port=5000)
 
